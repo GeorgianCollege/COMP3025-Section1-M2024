e64e9133021f6769cb6a0561053573441d8aa8c5
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-05-15 11:20:43 -0400

    added sharedEventHandler function

--- ICE2/app/src/main/java/ca/georgiancollege/ice2/MainActivity.kt
+++ ICE2/app/src/main/java/ca/georgiancollege/ice2/MainActivity.kt
@@ -2,6 +2,8 @@ package ca.georgiancollege.ice2
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -52,5 +54,10 @@ class MainActivity : AppCompatActivity()
 
             binding.helloWorldTextView.text = getString(R.string.hello_tom)
         }
+    }
+
+    fun sharedEventHandler(view: View)
+    {
+
     }
 }
