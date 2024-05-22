9973ed927925583c11f142ad2a5639cd56e2a818
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-05-22 09:31:16 -0400

    added project files for ICE3

---
+++ ICE3/app/src/main/java/ca/georgiancollege/ice3/MainActivity.kt
@@ -0,0 +1,61 @@
+package ca.georgiancollege.ice3
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ca.georgiancollege.ice3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        // instantiates an object of type ActivityMainBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        enableEdgeToEdge()
+
+        // sets the content view to the "super view" or main view group
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // creates a reference to the TextView
+        val helloWorldString = binding.helloWorldTextView
+        // changes the text property of the TextView
+        helloWorldString.text = getString(R.string.hello_tom)
+
+        val clickMeButton = binding.clickMeButton
+
+        clickMeButton.setOnClickListener{
+            Log.i("onCreate","Click Me Button Clicked!")
+
+            binding.helloWorldTextView.text = getString(R.string.clicked)
+        }
+
+        val anotherButton = binding.anotherButton
+
+        anotherButton.setOnClickListener {
+            Log.i("onCreate","Another Button was Clicked!")
+
+            binding.helloWorldTextView.text = getString(R.string.hello_tom)
+        }
+    }
+
+    fun sharedEventHandler(view: View)
+    {
+
+    }
+}
