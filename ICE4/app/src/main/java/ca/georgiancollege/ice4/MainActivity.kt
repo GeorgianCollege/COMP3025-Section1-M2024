67314b0fc029a2e6fd0681107409eeebdff3a921
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-05-29 10:20:09 -0400

    added Project files for ICE4

---
+++ ICE4/app/src/main/java/ca/georgiancollege/ice4/MainActivity.kt
@@ -0,0 +1,33 @@
+package ca.georgiancollege.ice4
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ca.georgiancollege.ice4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        // instantiates an object of type ActivityMainBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        enableEdgeToEdge()
+
+        // sets the content view to the "super view" or main view group
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
