6d2e28253e72dbf1487a564ea218448248a30cf1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-06-05 10:28:49 -0400

    added project files for ICE5

---
+++ ICE5/app/src/main/java/ca/georgiancollege/ice5/MainActivity.kt
@@ -0,0 +1,58 @@
+package ca.georgiancollege.ice5
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ca.georgiancollege.ice5.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        // instantiates an object of type ActivityMainBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        enableEdgeToEdge()
+
+        // sets the content view to the "super view" or main view group
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val operandButtons = arrayOf(
+            binding.oneButton, binding.twoButton, binding.threeButton, binding.fourButton,
+            binding.fiveButton, binding.sixButton, binding.sevenButton, binding.eightButton,
+            binding.nineButton, binding.zeroButton, binding.decimalButton, binding.deleteButton,
+            binding.plusMinusButton, binding.clearButton
+        )
+
+        val operatorButtons = arrayOf(
+            binding.mulitplyButton, binding.plusButton, binding.percentButton, binding.minusButton,
+            binding.equalsButton, binding.divideButton
+        )
+
+        operandButtons.forEach{it.setOnClickListener{operandHandler(it.tag as String)}}
+        operatorButtons.forEach{it.setOnClickListener{operatorHandler(it.tag as String)}}
+    }
+
+    private fun operandHandler(tag: String)
+    {
+        binding.resultTextView.text = tag
+    }
+
+    private fun operatorHandler(tag: String)
+    {
+        binding.resultTextView.text = tag
+    }
+}
