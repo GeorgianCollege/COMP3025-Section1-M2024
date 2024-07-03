9695708e10a75c0829bb37de690818a2df49a8c8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-03 10:33:38 -0400

    added Room Dependencies

--- ICE7/app/build.gradle.kts
+++ ICE7/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
@@ -40,12 +41,20 @@ android {
 }
 
 dependencies {
-
+    // Core Dependencies (mostly for UI)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Room Android Dependencies
+    implementation(libs.androidx.room.common)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
+
+    // Testing Dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
