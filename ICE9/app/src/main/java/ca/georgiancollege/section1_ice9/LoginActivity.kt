3bb1a6cdbc92e4942b53e8d4feb0d69d607bb730
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-17 09:58:19 -0400

    added Login and Register layouts

---
+++ ICE9/app/src/main/java/ca/georgiancollege/section1_ice9/LoginActivity.kt
@@ -0,0 +1,18 @@
+package ca.georgiancollege.section1_ice9
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ca.georgiancollege.section1_ice9.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
