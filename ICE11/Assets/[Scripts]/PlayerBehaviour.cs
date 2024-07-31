5e7757259371609d1524fc2b727cfdc24dc4202d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-31 10:04:45 -0400

    updated player movement to recieve touches

--- ICE11/Assets/[Scripts]/PlayerBehaviour.cs
+++ ICE11/Assets/[Scripts]/PlayerBehaviour.cs
@@ -5,6 +5,7 @@ public class PlayerBehaviour : MonoBehaviour
     public float max;
     public float min;
     public float horizontalSpeed;
+    public float verticalPosition;
 
     void Update()
     {
@@ -14,19 +15,29 @@ public class PlayerBehaviour : MonoBehaviour
 
     void Move()
     {
-        var x = Input.GetAxisRaw("Horizontal") * horizontalSpeed * Time.deltaTime;
-        transform.position += new Vector3(x, 0.0f, 0.0f);
+        // if there is at least one touch on the screen
+        if (Input.touchCount > 0)
+        {
+            // gets input touches from screen space in pixels
+            var x = Input.touches[0].position.x;
+
+            // convert screen space to world space (unity units)
+            var horizontalPosition = Camera.main.ScreenToWorldPoint(new Vector3(x, 0.0f, 0.0f)).x;
+
+            //var x = Input.GetAxisRaw("Horizontal") * horizontalSpeed * Time.deltaTime;
+            transform.position = new Vector3(horizontalPosition, verticalPosition, 0.0f);
+        }
     }
 
     void CheckBounds()
     {
         if (transform.position.x <= min)
         {
-            transform.position = new Vector3(min, -4.0f, 0.0f);
+            transform.position = new Vector3(min, verticalPosition, 0.0f);
         }
         else if (transform.position.x >= max)
         {
-            transform.position = new Vector3(max, -4.0f, 0.0f);
+            transform.position = new Vector3(max, verticalPosition, 0.0f);
         }
     }
 
