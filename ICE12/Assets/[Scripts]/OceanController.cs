96b0cc7199f838a7ba64dbf2891a0edc1409edce
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-08-07 09:22:10 -0400

    added Project files for ICE12

---
+++ ICE12/Assets/[Scripts]/OceanController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class OceanController : MonoBehaviour
+{
+    public float max;
+    public float min;
+    public float verticalSpeed;
+
+    void Start()
+    {
+        ResetGameObject();
+    }
+
+
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    void ResetGameObject()
+    {
+        transform.position = new Vector3(0.0f, max, 0.0f);
+    }
+
+    void Move()
+    {
+        transform.position += new Vector3(0.0f, -verticalSpeed * Time.deltaTime, 0.0f);
+    }
+
+    void CheckBounds()
+    {
+        if (transform.position.y <= min)
+        {
+            ResetGameObject();
+        }
+    }
+
+}
---
+++ ICE12/Assets/[Scripts]/PlayerBehaviour.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [Header("Movement Properties")]
+    public Boundary boundary;
+    public float verticalPosition;
+    [Header("Sound FX")]
+    public AudioSource yaySound;
+    public AudioSource thunderSound;
+
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    void Move()
+    {
+        // if there is at least one touch on the screen
+        if (Input.touchCount > 0)
+        {
+            // gets input touches from screen space in pixels
+            var x = Input.touches[0].position.x;
+
+            // convert screen space to world space (unity units)
+            var horizontalPosition = Camera.main.ScreenToWorldPoint(new Vector3(x, 0.0f, 0.0f)).x;
+
+            //var x = Input.GetAxisRaw("Horizontal") * horizontalSpeed * Time.deltaTime;
+            transform.position = new Vector3(horizontalPosition, verticalPosition, 0.0f);
+        }
+    }
+
+    void CheckBounds()
+    {
+        if (transform.position.x <= boundary.min)
+        {
+            transform.position = new Vector3(boundary.min, verticalPosition, 0.0f);
+        }
+        else if (transform.position.x >= boundary.max)
+        {
+            transform.position = new Vector3(boundary.max, verticalPosition, 0.0f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Island"))
+        {
+            yaySound.Play();
+        }
+        else if (other.gameObject.CompareTag("Cloud"))
+        {
+            thunderSound.Play();
+        }
+    }
+
+}
